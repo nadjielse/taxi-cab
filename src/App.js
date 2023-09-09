542250abc83046b765d31ea17533d801168f6510
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-09-09 14:29:26 -0300

    Adicionando html inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h2>Encontre Fábio no local de partida</h2>
+      <p>Chega em 3 minutos (800 metros)</p>
+      <div>
+        <div>
+          <div>
+            <img src="" title="" alt="" />
+            <span>★ 5.0</span>
+          </div>
+          <h4>Fábio D.</h4>
+        </div>
+        <img src="" title="" alt="" />
+        <div>
+          <h3>BCD0D19</h3>
+          <p>Honda Civic Roxo</p>
+        </div>
+      </div>
+      <form action="" method="">
+        <input id="message" type="text" placeholder="Enviar mensage para Fábio..."></input>
+        <button type="submit">Publicar</button>
+        <button></button>
+        <button></button>
+      </form>
+      <div>
+        <img src="" title="" alt="" />
+        <p>Boris estou no local já lhe esperando. Estou com camisa azul e calça jeans.</p>
+        <button></button>
+      </div>
+      <div>
+        <img src="" title="" alt="" />
+        <p>Acabei de lhe ver. Vou estacionar o carro próximo.</p>
+        <button></button>
+      </div>
+    </main>
   );
 }
 
