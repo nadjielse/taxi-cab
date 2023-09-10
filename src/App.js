625d2f9ee28f80ac87ed501b34490ae6874c8e13
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-09-10 18:05:11 -0300

    Adicionando o css

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 import './App.css';
 
+import car from "./assets/car.png";
+import driver from "./assets/driver.png";
+import user from "./assets/user.png";
+import phone from "./assets/phone.svg";
+import shield from "./assets/shield.svg";
+import trashcan from "./assets/trash-can.svg";
+
 function App() {
   return (
-    <main>
-      <h2>Encontre Fábio no local de partida</h2>
-      <p>Chega em 3 minutos (800 metros)</p>
-      <div>
-        <div>
-          <div>
-            <img src="" title="" alt="" />
-            <span>★ 5.0</span>
+    <main id="container">
+      <header id="header"></header>
+      <h2 id="title">Encontre <span id="highlight">Fábio</span> no local de partida</h2>
+      <p id="subtitle">Chega em 3 minutos (800 metros)</p>
+      <div id="about">
+        <div id="profile">
+          <div id="picture">
+            <img src={ driver } title="" alt="" />
+            <span id="rate">★ 5.0</span>
           </div>
-          <h4>Fábio D.</h4>
+          <h4 id="driver-name">Fábio D.</h4>
         </div>
-        <img src="" title="" alt="" />
+        <img id="car" src={ car } title="" alt="" />
         <div>
-          <h3>BCD0D19</h3>
-          <p>Honda Civic Roxo</p>
+          <h3 id="plate">BCD0D19</h3>
+          <p id="car-name">Honda Civic Roxo</p>
         </div>
       </div>
-      <form action="" method="">
-        <input id="message" type="text" placeholder="Enviar mensage para Fábio..."></input>
-        <button type="submit">Publicar</button>
-        <button></button>
-        <button></button>
+      <form id="form" action="" method="">
+        <input id="message-field" type="text" placeholder="Enviar mensagem para Fábio..."></input>
+        <button id="send-message" type="submit">Publicar</button>
+        <button className="contact"><img src={ phone } title="" alt="" /></button>
+        <button className="contact"><img src={ shield } title="" alt="" /></button>
       </form>
-      <div>
-        <img src="" title="" alt="" />
+      <div className="comment">
+        <img className="user-pic" src={ user } title="" alt="" />
         <p>Boris estou no local já lhe esperando. Estou com camisa azul e calça jeans.</p>
-        <button></button>
+        <button className="trash"><img src={ trashcan } title="" alt="" /></button>
       </div>
-      <div>
-        <img src="" title="" alt="" />
+      <div className="comment">
+        <img className="user-pic" src={ driver } title="" alt="" />
         <p>Acabei de lhe ver. Vou estacionar o carro próximo.</p>
-        <button></button>
+        <button className="trash"><img src={ trashcan } title="" alt="" /></button>
       </div>
     </main>
   );
